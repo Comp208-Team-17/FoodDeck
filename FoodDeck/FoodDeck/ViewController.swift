97001db28b0f4fbcd53242a97b5a490a57507806
sara-e24 <saraelkehya@gmail.com>
2020-03-02 12:12:56 +0000

    created initial Xcode project

---
+++ FoodDeck/FoodDeck/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodDeck
+//
+//  Created by Elkehya, Sara Mruan M on 02/03/2020.
+//  Copyright © 2020 computer science. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
