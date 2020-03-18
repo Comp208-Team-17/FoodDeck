6dc981090873764e2764ed49af3a8c807afc0aeb
sara-e24 <saraelkehya@gmail.com>
2020-03-18 14:21:39 +0000

    Update tab bars and nav bars

--- FoodDeck/FoodDeck/AppDelegate.swift
+++ FoodDeck/FoodDeck/AppDelegate.swift
@@ -16,6 +16,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        // tab bar colours
+        UITabBar.appearance().barTintColor = .black
+        UITabBar.appearance().tintColor = .green
         return true
     }
 
