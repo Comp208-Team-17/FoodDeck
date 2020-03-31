21403c8cfc9ccbeeebcbbfac13e86b372448cf55
Luke Wood <luke@lukenet.co.uk>
2020-03-31 15:55:48 +0100

    Ingredients manager
    - Saving new records to database
    - Fetching an array of ingredients from database
    
    Still working on:
    Deleting ingredient from database.
    Note: Check impact on referential integrity?? Look this up

---
+++ FoodDeck/FoodDeck/IngredientsManager.swift
@@ -0,0 +1,63 @@
+//
+//  IngredientsManager.swift
+//  FoodDeck
+//
+//  Created by Luke Wood on 31/03/2020.
+//  Copyright © 2020 computer science. All rights reserved.
+//
+import CoreData
+import UIKit
+class IngredientsManager: NSManagedObject {
+    struct Ingredient{
+        var name : String
+        var unit : String
+        var enabled : Bool
+        init(theName : String, theUnit : String, isEnabled : Bool){
+            name = theName
+            unit = theUnit
+            enabled = isEnabled
+        }
+    }
+    var ingredients : [Ingredient] = []
+    
+    func updateIngredients(){
+        ingredients.removeAll()
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {return}
+        let managedContext = appDelegate.persistentContainer.viewContext
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "ingreident")
+        request.returnsObjectsAsFaults = false
+        do{
+            let result = try managedContext.fetch(request)
+            for data in result as! [NSManagedObject] {
+                ingredients.append(Ingredient(theName: data.value(forKey:"name") as! String, theUnit: data.value(forKey:"unit") as! String, isEnabled: data.value(forKey:"enabled") as! Bool) )
+            }
+           
+        }
+        catch{
+            print("updating ingredients array failed")
+        }
+    }
+    func deleteIngredient(theName : String){
+        
+    }
+    func addIngredient(theName : String, theUnit : String, isEnabled: Bool){
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {return}
+        let managedContext = appDelegate.persistentContainer.viewContext
+        let ingredientEntity = NSEntityDescription.entity(forEntityName: "Ingredent", in: managedContext)!
+        let anIngredient = NSManagedObject(entity: ingredientEntity, insertInto: managedContext)
+        anIngredient.setValue(theName, forKey: "name")
+        anIngredient.setValue(theUnit, forKey: "unit")
+        anIngredient.setValue(isEnabled, forKey: "enabled")
+        do {
+            try managedContext.save()
+           } catch {
+            print("Failed saving")
+         }
+        updateIngredients()
+    }
+}
+
+    
+    
+
+
