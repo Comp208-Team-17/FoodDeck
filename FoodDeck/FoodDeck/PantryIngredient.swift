1ff2d11be43180c814ed384d61f40474c5c17561
sara-e24 <S.Elkehya@student.liverpool.ac.uk>
2020-04-18 20:59:01 +0100

    added method to return all pantry ingredients

--- FoodDeck/FoodDeck/PantryIngredient.swift
+++ FoodDeck/FoodDeck/PantryIngredient.swift
@@ -8,8 +8,27 @@
 
 import Foundation
 import CoreData
+import UIKit
 
 @objc(PantryIngredient)
 
 class PantryIngredient: NSManagedObject {
+    
+    // return all ingredients in the pantry
+    static func getAllPanryIngredients() -> [Ingredient] {
+        var inPantry : [PantryIngredient] = []
+        var pantryList : [Ingredient] = []
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let managedContext = appDelegate.persistentContainer.viewContext
+        let fetchPantry: NSFetchRequest<PantryIngredient>  = PantryIngredient.fetchRequest()
+        do {
+            inPantry = try managedContext.fetch(fetchPantry)
+        } catch {
+            print("could not retrieve ingredients")
+        }
+        for index in 0..<inPantry.count {
+            pantryList.append(inPantry[index].belongsTo!)
+        }
+        return pantryList
+    }
 }
