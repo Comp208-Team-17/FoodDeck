560f7514e9ed1cfef02b3ab47846d20527d9f4d5
Luke Wood <luke@lukenet.co.uk>
2020-04-21 11:40:05 +0100

    Added options for picker view on EditRecipeViewController

--- FoodDeck/FoodDeck/Recipes/EditRecipeViewController.swift
+++ FoodDeck/FoodDeck/Recipes/EditRecipeViewController.swift
@@ -17,28 +17,55 @@ class EditRecipeViewController: UIViewController {
     @IBOutlet weak var txtName: UITextField!
     @IBOutlet weak var txtDescription: UITextView!
     @IBOutlet weak var txtInstructions: UITextView!
+    @IBOutlet weak var btnChooseIngredients: UIButton!
+    static var dietary : String = ""
+    let pickerOptions : [String] = ["Breakfast", "Lunch", "Dinner"]
+    var pickerOptionSet : String = "Breakfast"
+    var chooseIngredients = false
     @IBAction func btnSave(_ sender: Any) {
-        /*
+        var saveComplete : Bool = false
         if RecipeViewController.addButton == true {
-            let dietary = RecipeManager.convertDietaryValue(vegan: swVegan.isOn, veg: swVegetarian.isOn, gluten: swGluten.isOn)
             var cookTimeTmp : Int16?
             var prepTimeTmp : Int16?
             var servingsTmp : Int16?
             if let cookTime = Int16(txtCookTime.text!){
                 cookTimeTmp? = cookTime
             }
+            else{
+                saveComplete = false
+            }
             if let prepTime = Int16(txtPrepTime.text!){
                 prepTimeTmp? = prepTime
             }
+            else{
+                saveComplete = false
+            }
             if let servings = Int16(txtServings.text!){
-                
+                servingsTmp? = servings
             }
-            if RecipeManager.addRecipe(theAllergens: "", isAvailable: true, theCookTime: cookTimeTmp ?? 0, theDateCreated: "", theDietaryRequirements: dietary, isFavourite: false, theInstructions: txtInstructions.text!, theName: txtName.text!, thePrepTime: prepTimeTmp, theRating: 0, theRecipeDescription: txtDescription.text, theScore: 100, theServings: Int16, theThumbnail: UIImage(), theTimeOfDay: <#T##String#>, theIngredients: <#T##[(String, Int16, Bool)]#>){
-                
+            else {
+                saveComplete = false
             }
-        }
- */ 
             
+            if saveComplete == true {
+                if RecipeManager.addRecipe(theAllergens: "", isAvailable: true, theCookTime: cookTimeTmp ?? 0, theDateCreated: "", theDietaryRequirements: EditRecipeViewController.dietary, isFavourite: false, theInstructions: txtInstructions.text!, theName: txtName.text!, thePrepTime: prepTimeTmp!, theRating: 0, theRecipeDescription: txtDescription.text, theScore: 100, theServings: servingsTmp!, theThumbnail: UIImage(), theTimeOfDay: pickerOptionSet, theIngredients: []){
+                             chooseIngredients = true
+                    btnChooseIngredients.isEnabled = true
+                         }
+                else {
+                    //display error message about saving recipe
+                }
+                
+            }
+            else {
+                //display error message
+            }
+         
+        }
+        else {
+            //load ingredient information
+            chooseIngredients = true
+        }
     }
     
     override func viewDidLoad() {
@@ -57,6 +84,9 @@ class EditRecipeViewController: UIViewController {
             }
             
         }
+        if RecipeViewController.addButton == true {
+            btnChooseIngredients.isEnabled = false
+        }
     }
 
     /*
@@ -69,4 +99,20 @@ class EditRecipeViewController: UIViewController {
     }
     */
 
+}
+extension EditRecipeViewController : UIPickerViewDataSource, UIPickerViewDelegate {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerOptions.count
+    }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerOptions[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        pickerOptionSet = pickerOptions[row]
+    }
 }
